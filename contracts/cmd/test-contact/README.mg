796fea2449b0ef658b488d5340a5b31b49a95ff4
regnull <regnull@gmail.com>
2021-12-29 21:39:52 -0500

    test smart contract example

---
+++ contracts/cmd/test-contact/README.mg
@@ -0,0 +1,11 @@
+# Test Smart Contract
+
+This example shows how to dest a smart contract on a simulated blockchain.
+
+See also: https://medium.com/hackernoon/a-step-by-step-guide-to-testing-and-deploying-ethereum-smart-contracts-in-go-9fc34b178d78
+and https://stackoverflow.com/questions/69934685/transaction-type-not-supported-when-trying-to-deploy-a-simple-contract-using-g
+
+To run:
+```
+go run main.go
+```
---
+++ contracts/cmd/test-contact/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind/backends"
+	"github.com/ethereum/go-ethereum/core"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/regnull/go-ethereum-examples/contracts/store"
+)
+
+func main() {
+	privateKey, err := crypto.GenerateKey()
+	if err != nil {
+		log.Fatal(err)
+	}
+	auth := bind.NewKeyedTransactor(privateKey)
+	alloc := make(core.GenesisAlloc)
+	// Balance should be high enough to cover the transaction costs.
+	alloc[auth.From] = core.GenesisAccount{Balance: big.NewInt(1000000000000000)}
+	blockchain := backends.NewSimulatedBackend(alloc, 10000000)
+	gasPrice, err := blockchain.SuggestGasPrice(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	auth.GasPrice = gasPrice
+	address, tx, instance, err := store.DeployStore(auth, blockchain, "v12.34")
+	blockchain.Commit()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("contract address: %s\n", address.String())
+	fmt.Printf("deploy tx: %s\n", tx.Hash().Hex())
+
+	key := [32]byte{}
+	value := [32]byte{}
+	copy(key[:], []byte("hello"))
+	copy(value[:], []byte("world"))
+
+	tx, err = instance.SetItem(auth, key, value)
+	if err != nil {
+		log.Fatal(err)
+	}
+	blockchain.Commit()
+
+	fmt.Printf("tx sent: %s\n", tx.Hash().Hex())
+
+	result, err := instance.Items(nil, key)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("items: %x\n", result)
+}
